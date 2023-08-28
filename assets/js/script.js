52434fd99b7f2daf419fa033cc67a5c444ae6d1a
Arundurai29 <114252340+Arundurai29@users.noreply.github.com>
2023-08-28 15:40:40 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      items: 3,
+      loop: true,
+      nav: true,
+      dots: false,
+      navText: ["<i class='fa-solid fa-arrow-left'></i>", "<i class='fa-solid fa-arrow-right'></i>"],
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:2
+        },
+        1000:{
+          items:3
+        }
+      }
+    });
+  });
